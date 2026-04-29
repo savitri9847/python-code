0381e6aa14cf48061b8833aa1fed076ddc6b5bd7
savitrikashyap@12 <savitrikashyap452@gmail.com>
2026-04-29 15:11:02 +0530

    I have updated all new file

---
+++ eleven.py
@@ -0,0 +1,16 @@
+import numpy as np
+# arr1 = np.array([1,2,3])
+# arr2 = np.array([4,5,6])
+# newarr = np.add(arr1,arr2)
+# print("Addition is",newarr )
+# # summation
+
+# arr3 =  np.array([1,2,3])
+# arr4 = np.array([4,5,6])
+# newarr1 = np.sum([arr3,arr4])
+# print("summation is ", newarr1)
+
+# cumulative summation
+arr = np.array([1,2,3,4,5,6])
+newarr2 = np.cumsum(arr)
+print("cumulative is ", newarr2)
